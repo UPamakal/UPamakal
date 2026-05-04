b3e02a7ed81bcd6c536225b1b8dc1f10eb33f89d
Stefan Niedes <sgniedes@up.edu.ph>
2026-05-04 22:18:16 +0800

    chore: added firebase core dependency

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,16 @@
 import 'package:flutter/material.dart';
+import 'package:firebase_core/firebase_core.dart';
+import 'firebase_options.dart';
 
-void main() {
+void main() async {
+  // 1. Ensure Flutter is initialized
+  WidgetsFlutterBinding.ensureInitialized();
+
+  // 2. Initialize Firebase using the generated options
+  await Firebase.initializeApp(
+    options: DefaultFirebaseOptions.currentPlatform,
+  );
+
   runApp(const MyApp());
 }
 
